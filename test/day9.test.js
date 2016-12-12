92f1b35443b28531ab27c5f9f64177090dc843f5
Stoyan Mishkovski <mishkovski@gmail.com>
2016-12-12 13:09:24 -0500

    day9 puzzle 2

--- test/day9.test.js
+++ test/day9.test.js
@@ -34,18 +34,56 @@ describe('day9', function () {
     })
 
     it('findMarker works for X(8x2)(3x3)ABCY', function () {
-        day9.getExtraLength('X(8x2)(3x3)ABCY');
-        //assert.equal(day9.findMarker('X(8x2)(3x3)ABCY'), 20);
+        assert.deepEqual(day9.findMarker('X(8x2)(3x3)ABCY'),
+            { found: true, content: '8x2', phraseLength: 8, miltiplier: 2 });
     });
 
-    it.only('getExtraLength works for X(8x2)(3x3)ABCY', function () {
-        assert.equal(day9.getExtraLength('X(8x2)(3x3)ABCY'), 20);
+    it('findMarker works for X(80x20)(3x3)ABCY', function () {
+        assert.deepEqual(day9.findMarker('X(80x20)(3x3)ABCY'),
+            { found: true, content: '80x20', phraseLength: 80, miltiplier: 20 });
     });
 
+    it('findSubstring works for X(8x2)(3x3)ABCY', function () {
+        assert.deepEqual(day9.findSubstring({ found: true, content: '8x2', phraseLength: 8, miltiplier: 2 },
+            'X(8x2)(3x3)ABCY'),
+            '(3x3)ABC');
+    });
 
+    it('findSubstring works for X(9x2)(3x3)ABCY', function () {
+        assert.deepEqual(day9.findSubstring({ found: true, content: '9x2', phraseLength: 9, miltiplier: 2 },
+            'X(9x2)O(3x3)ABCY'),
+            'O(3x3)ABC');
+    });
 
-    it('getExtraLength works correctly for the puzzle input', function () {
-        var input = readFile('./test/day9.input');
-        printResult(day9.getExtraLength(input));
-    })
+    it('findSubstring works for XZ(10x2)E(3x3)ABCGYO', function () {
+        assert.deepEqual(day9.findSubstring({ found: true, content: '9x2', phraseLength: 9, miltiplier: 2 },
+            'X(9x2)O(3x3)ABCY'),
+            'O(3x3)ABC');
+    });
+
+    it('findSubstring works for (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', function () {
+        assert.deepEqual(day9.findSubstring({ found: true, content: '25x3', phraseLength: 25, miltiplier: 3 },
+            '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'),
+            '(3x3)ABC(2x3)XY(5x2)PQRST');
+    });
+
+    it('calculateLength works for X(8x2)(3x3)ABCY', function () {
+        assert.equal(day9.calculateLength('X(8x2)(3x3)ABCY', 1), 20);
+    });
+
+    it('calculateLength works for XZ(10x2)E(3x3)ABCGYO', function () {
+        assert.equal(day9.calculateLength('XZ(10x2)E(3x3)ABCGYO', 1), 26);
+    });
+
+    it('calculateLength works for (3x3)XYZ', function () {
+        assert.equal(day9.calculateLength('(3x3)XYZ', 1), 9);
+    });
+
+    it('calculateLength works for (27x12)(20x12)(13x14)(7x10)(1x12)A', function () {
+        assert.equal(day9.calculateLength('(27x12)(20x12)(13x14)(7x10)(1x12)A', 1), 241920);
+    });
+
+    it.only('calculateLength works for (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', function () {
+        assert.equal(day9.calculateLength('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 1), 445);
+    });
 });
